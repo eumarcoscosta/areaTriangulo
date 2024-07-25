43fd2e6d68a727790e113746384f6fef6595e83a
Marcos Costa <marcoscosta@air-de-marcos.lan>
2024-07-25 09:13:32 +0100

    Complete app

---
+++ areaTriangulo/areaTriangulo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Triangulo x, y;
+
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do triângulo X:");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triângulo Y:");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            // método atualizado
+            // double areaX = x.Area();
+            // double areaY = y.Area();
+
+            double p = (x.A + x.B + x.C) / 2.0;
+            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+
+            p = (y.A + y.B + y.C) / 2.0;
+            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+
+            Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de Y = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior área: X");
+            } else
+            {
+                Console.WriteLine("Maior área: Y");
+            }
+
+        }
+    }
+}
---
+++ areaTriangulo/areaTriangulo/Triangulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace course
+{
+	public class Triangulo
+	{
+
+		public double A;
+		public double B;
+		public double C;
+
+		public double Area()
+		{
+			double p = (A + B + C) / 2.0;
+			return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+
+		}
+
+		public Triangulo()
+		{
+		}
+	}
+}
+
